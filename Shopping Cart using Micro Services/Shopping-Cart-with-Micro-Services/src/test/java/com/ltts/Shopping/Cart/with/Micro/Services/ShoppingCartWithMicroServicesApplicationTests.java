83db71498d72c1ec7e0b8f2c52c0f87a3caac24b
Kricha Sahu <78844495+PS99003587@users.noreply.github.com>
2021-03-29 10:31:15 +0530

    Add files via upload

---
+++ Shopping Cart using Micro Services/Shopping-Cart-with-Micro-Services/src/test/java/com/ltts/Shopping/Cart/with/Micro/Services/ShoppingCartWithMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.Shopping.Cart.with.Micro.Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartWithMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
